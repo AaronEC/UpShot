c1c3c97894154ad39c2e6da31e76d74a9f903f82
Aaron Cardwell <aaron_cardwell@hotmail.com>
2022-03-07 12:58:58 +0000

    Initial Commit

---
+++ UpShot/UpShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  UpShotApp.swift
+//  UpShot
+//
+//  Created by Aaron Cardwell on 07/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct UpShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
