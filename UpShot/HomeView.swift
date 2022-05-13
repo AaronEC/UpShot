d9c25cde8c5f808d25c9283d5639740893f6a60f
AaronEC <aaron_cardwell@hotmail.com>
2022-05-13 23:29:10 +0100

    Check for marking

--- UpShot/HomeView.swift
+++ UpShot/HomeView.swift
@@ -14,9 +14,9 @@ struct HomeView: View {
     var body: some View {
         NavigationView {
             VStack{
-                
+
                 HomePageView()
-                
+
                 TextField(
                     "Archer's Name",
                     text: $userName
@@ -29,101 +29,462 @@ struct HomeView: View {
                     .shadow(radius: 10)
                     .frame(width: 280, height: 50)
                     .multilineTextAlignment(.center)
-                
+
                 NavigationLink(destination: CharacterSelectView(userName: userName), label: {Text("Start Match")
                         .bold()
                         .frame(width: 280, height: 50)
-                        .background(Color.green)
+                        .background(Color("AccentColour"))
                         .foregroundColor(.white)
                         .cornerRadius(10)
                 })
-                
-                NavigationLink(destination: OptionsView(color: .red), label: {Text("Options")
+
+                NavigationLink(destination: OptionView(color: .red), label: {Text("Options")
                         .bold()
                         .frame(width: 280, height: 50)
-                        .background(Color.red)
+                        .background(Color("MainColour"))
                         .foregroundColor(.white)
                         .cornerRadius(10)
                 })
-                
+
                 Spacer()
             }
             .navigationBarTitle("")
-            .navigationBarBackButtonHidden(true)
-            .navigationBarHidden(true)
         }
         .accentColor(Color(.label))
-        
-        
-    }
-}
-
-struct MatchView: View {
-    
-    var userName: String
-    
-    var body: some View {
-        VStack{
-            
-            Text("Match View")
-
-        }
     }
 }
 
 struct CharacterSelectView: View {
-    
-    var noOfImages = 4
+
+    @State private var selectedTab = "0"
+    var noOfImages = 3
     var userName: String
-    
+    var charNames:[String] = ["Joey","Sarah", "Aiza"]
+
     var body: some View {
-        NavigationView{
             VStack {
                 Text("Welcome " + userName)
-                    .padding()
                     .font(.system(size: 32, weight: .bold))
                     .multilineTextAlignment(.center)
-                
+
                 GeometryReader{ proxy in
-                    TabView {
-                        
+
+                    TabView (selection: $selectedTab){
+
                         ForEach(0..<noOfImages) {num in
-                            Image("Character" + String(num))
-                                .resizable()
-                                .scaledToFit()
-                                .tag(num)
+                            VStack{
+                                Image("Character" + String(num))
+                                    .resizable()
+                                    .scaledToFit()
+                                
+                                Text(charNames[num])
+                                Image("Star" + String(num))
+                                    .resizable()
+                                    .frame(width: 192, height: 64)
+                            }
+                            .tag(String(num))
+                            .padding(.bottom, 50)
                         }
-                        
+
                     }
                     .tabViewStyle(PageTabViewStyle())
                     .padding()
-                    .accentColor(Color(.label))
                     .frame(width: proxy.size.width, height: proxy.size.height, alignment: .center)
+                    .indexViewStyle(.page(backgroundDisplayMode: .always))
+                    .background(Color(.systemGroupedBackground))
                 }
+
                 Text("Select your opponent!")
                     .padding()
                     .font(.system(size: 24, weight: .bold))
-                
-                NavigationLink(destination: CharacterSelectView(userName: userName), label: {Text("Begin Match!")
+
+                NavigationLink(destination: MatchView(userName: userName, charNames: charNames, charID: selectedTab), label: {Text("Begin Match!")
                         .bold()
                         .frame(width: 280, height: 50)
-                        .background(Color.green)
+                        .background(Color("AccentColour"))
                         .foregroundColor(.white)
                         .cornerRadius(10)
                         .padding()
                 })
-            }
-            .navigationBarTitle("")
-            .navigationBarBackButtonHidden(true)
-            .navigationBarHidden(true)
         }
     }
 }
 
-struct OptionsView: View {
+enum scoreButtons: String {
+    case one = "1"
+    case two = "2"
+    case three = "3"
+    case four = "4"
+    case five = "5"
+    case six = "6"
+    case seven = "7"
+    case eight = "8"
+    case nine = "9"
+    case ten = "10"
+    case miss = "M"
     
+    var buttonColour: Color {
+        switch self {
+        case .ten, .nine:
+            return .yellow
+        case .eight, .seven:
+            return .red
+        case .six, .five:
+            return .blue
+        case .four, .three:
+            return .black
+        case .two, .one:
+            return .white
+        case .miss:
+            return Color(.lightGray)
+        }
+    }
+    
+    var numberColour: Color {
+        switch self {
+        case .ten, .nine, .two, .one:
+            return .black
+        default:
+            return .white
+        }
+    }
+    
+}
+
+struct MatchView: View {
+    
+    @State var value = "------"
+    @State var total = 0
+    
+    @State var oppValue = "------"
+    @State var oppTotal = 0
+    
+    @State var message = ""
+    @State private var visible = true
+    @State private var shoot = true
+    @State private var complete = false
+    
+    var userName: String
+    var charNames:[String]
+    var charID: String
+    var buttons: [[scoreButtons]] = [
+        [.ten, .nine, .eight],
+        [.seven, .six, .five],
+        [.four, .three, .two],
+        [.miss, .one]
+    ]
+
+    var body: some View {
+        ZStack{
+                        
+            VStack{
+                
+                Text(charNames[Int(charID) ?? 0] + " Vs " + userName)
+                    .font(.system(size: 32, weight: .bold))
+                    .multilineTextAlignment(.center)
+                
+                Spacer()
+                
+                // Score Display
+                HStack {
+                    
+                    GeometryReader { geo in
+                        Image("Character" + charID)
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: geo.size.width * 0.25)
+                    }
+                    
+                    // Opponent
+                    VStack {
+                        ForEach(Array(oppValue), id: \.self) { character in
+                            if character == "x" {
+                                Text("10")
+                                    .bold()
+                                    .font(.system(size: 32))
+                                    .foregroundColor(Color("MainColour"))
+                            } else {
+                                Text(String(character))
+                                    .bold()
+                                    .font(.system(size: 32))
+                                    .foregroundColor(Color("MainColour"))
+                            }
+                        }
+                        Spacer()
+                        Text("Total:")
+                        Text(String(oppTotal))
+                            .bold()
+                            .font(.system(size: 58))
+                        Spacer()
+                    }
+                    .padding(.trailing, 15)
+                    
+                    Spacer()
+                    
+                    // Player
+                    VStack {
+                        ForEach(Array(value), id: \.self) { character in
+                            if character == "x" {
+                                Text("10")
+                                    .bold()
+                                    .font(.system(size: 32))
+                                    .foregroundColor(Color("MainColour"))
+                            } else {
+                                Text(String(character))
+                                    .bold()
+                                    .font(.system(size: 32))
+                                    .foregroundColor(Color("MainColour"))
+                            }
+                        }
+                        Spacer()
+                        Text("Total:")
+                        Text(String(total))
+                            .bold()
+                            .font(.system(size: 58))
+                        Spacer()
+                    }
+                    .padding(.leading, 15)
+                    
+                    VStack {
+                        Spacer()
+                        GeometryReader { geo in
+                            Image("Target")
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: geo.size.width * 0.15, height: geo.size.height * 0.3)
+                                .padding([.top, .leading], 50)
+                        }
+                    }
+                    .padding(.top, 50)
+                }
+
+                if complete {
+                    NavigationLink(destination: ResultsView(result: getResult(), score: getScore(), username: userName), label: {Text("Results Page")
+                            .bold()
+                            .frame(width: 280, height: 50)
+                            .background(Color("AccentColour"))
+                            .foregroundColor(.white)
+                            .cornerRadius(10)
+                            .padding()
+                    })
+                } else if shoot {
+                    Text("Enter your score:")
+                } else {
+                    VStack {
+                        Text(charNames[Int(charID) ?? 0] + " is shooting...")
+                            .opacity(visible ? 0 : 1)
+                    }
+                }
+                            
+                Spacer()
+
+                // Input Buttons
+                ForEach(buttons, id: \.self) { row in
+                    HStack {
+                        ForEach(row, id: \.self) {item in
+                            Button(action: {
+                                self.didTap(button: item)
+                            }, label: {
+                                Text(item.rawValue)
+                                    .font(.system(size: 28))
+                                    .frame(
+                                        width: self.buttonWidth(item: item),
+                                        height: self.buttonHeight()
+                                    )
+                                    .background(item.buttonColour)
+                                    .foregroundColor(item.numberColour)
+                                    .overlay(
+                                        RoundedRectangle(
+                                            cornerRadius: 15
+                                        )
+                                            .stroke(Color(.black), lineWidth: 4)
+                                        )
+                            })
+                        }
+                        .cornerRadius(15)
+                    }
+                    .padding(.horizontal, 20)
+                    .disabled(!visible)
+                }
+            }
+        }
+    }
+    
+    private func pulsateText() {
+            withAnimation(Animation.easeInOut.repeatCount(14, autoreverses: false)) {
+                visible.toggle()
+            }
+        }
+    
+    func getResult() -> String {
+        
+        var result = ""
+        
+        if total == oppTotal {
+            result = "Draw"
+        } else if total > oppTotal {
+            result = "Win"
+        } else {
+            result = "Lose"
+        }
+        
+        return result
+    }
+    
+    func getScore() -> String {
+               
+        return String(oppTotal) + " - " + String(total)
+    }
+    
+    func oppScore() {
+                
+        let randomInt = Int.random(in: 0..<11)
+        
+        if randomInt == 10 {
+            self.oppValue.removeFirst()
+            self.oppValue = self.oppValue + "x"
+            self.oppTotal += 10
+        } else if randomInt == 0 {
+            self.oppValue.removeFirst()
+            self.oppValue = self.oppValue + "M"
+            self.oppTotal += randomInt
+        } else {
+            self.oppValue.removeFirst()
+            self.oppValue = "\(self.oppValue)\(randomInt)"
+            self.oppTotal += randomInt
+        }
+        
+        if self.value.first != "-" {
+            complete.toggle()
+            return
+        }
+
+    }
+    
+    func didTap(button: scoreButtons) {
+        
+        shoot = false
+        
+        class TryThis {
+            func getSomethingLater(_ number: Double) async -> String {
+                // sleep for 3 seconds, then return
+                Thread.sleep(forTimeInterval: 3)
+                return String(format: ">>>%8.2f<<<", number)
+            }
+        }
+
+        let tryThis = TryThis()
+        
+        let number = button.rawValue
+        
+        if self.oppValue.first != "-" {
+            return
+        }
+        
+        if number == "10" {
+            self.value.removeFirst()
+            self.value = self.value + "x"
+            self.total += 10
+        } else {
+            self.value.removeFirst()
+            self.value = "\(self.value)\(number)"
+            self.total += Int(number) ?? 0
+        }
+        
+        pulsateText()
+        
+        Task {
+            await tryThis.getSomethingLater(1)
+            oppScore()
+            visible.toggle()
+            shoot = true
+        }
+    }
+    
+    func buttonWidth(item: scoreButtons) -> CGFloat {
+        if item.rawValue == "M" {
+            return (UIScreen.main.bounds.width) / 2 + 8
+        }
+        return (UIScreen.main.bounds.width) / 4
+    }
+    func buttonHeight() -> CGFloat {
+        return (UIScreen.main.bounds.width) / 7
+    }
+}
+
+
+struct ResultsView: View {
+    
+    var result: String
+    var score: String
+    var username: String
+    
+    var messages = [
+        
+        ["Win": "Congratulations ",
+        "Lose": "Unlucky ",
+        "Draw": "So Close "],
+        
+        ["Win": "You Won!",
+        "Lose": "Keep trying!",
+        "Draw": "You Drew"],
+        
+        ]
+    
+    var code = ["Win": 0,
+                "Lose": 1,
+                "Draw": 2]
+
+    var body: some View {
+        VStack {
+            
+            Text(messages[0][result] ?? "Draw" + username)
+                .font(.system(size: 32, weight: .bold))
+                .multilineTextAlignment(.center)
+            
+            Text(messages[1][result] ?? "Draw")
+                .font(.system(size: 64, weight: .bold))
+                .multilineTextAlignment(.center)
+            
+            Image("result" + String(code[result] ?? 2))
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .padding(.top, 20)
+                .scaledToFit()
+            
+            Text("Your Score Was:")
+                .font(.system(size: 44, weight: .bold))
+                .multilineTextAlignment(.center)
+            
+            Text(score)
+                .font(.system(size: 32, weight: .bold))
+                .multilineTextAlignment(.center)
+            
+            NavigationLink(destination: CharacterSelectView(userName: username), label: {Text("New Match")
+                    .bold()
+                    .frame(width: 280, height: 50)
+                    .background(Color("AccentColour"))
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            })
+
+            NavigationLink(destination: HomeView(), label: {Text("Home Screen")
+                    .bold()
+                    .frame(width: 280, height: 50)
+                    .background(Color("MainColour"))
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            })
+            
+        }
+        .padding()
+    }
+}
+
+struct OptionView: View {
+
     var color: Color
-    
+
     var body: some View {
         VStack{
             Text("Options Page")
@@ -131,65 +492,59 @@ struct OptionsView: View {
     }
 }
 
+
 struct HomePageView: View {
     var body: some View {
-        
-        Image("Logo_trans")
+
+        Image("cover")
             .resizable()
             .aspectRatio(contentMode: .fit)
-            .padding(.all)
+            .padding(.top, 20)
             .scaledToFit()
-            .frame(width: 320)
-            .shadow(radius: 10)
-        
-        HStack {
-            Image("Character2flip")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .padding(.leading, 40)
-                .padding(.trailing, -10)
-                .scaledToFit()
-            Image("Character1")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .padding(.trailing, 40)
-                .padding(.leading, -10)
-                .scaledToFit()
-        
-        }
-        
+
         Spacer()
     }
 }
 
-struct TempView: View {
-    
-    var color: Color
-    var number: Int
-    
+struct TestView: View {
+
+    @State private var selectedTab = "1"
+
     var body: some View {
-        Text("Match Page")
+
+        VStack{
+        TabView(selection: $selectedTab) {
+            Text("Tab 1")
+                .onTapGesture {
+                    selectedTab = "2"
+                }
+                .tag("2")
+            Text("Tab 2")
+                .onTapGesture {
+                    selectedTab = "3"
+                }
+                .tag("3")
+            Text("Tab 3")
+                .onTapGesture {
+                    selectedTab = "4"
+                }
+                .tag("4")
+        }
+        .tabViewStyle(PageTabViewStyle())
+        Text(selectedTab)
+
+        }
     }
-//    var color: Color
-//    var number: Int
-//
-//    var body: some View {
-//        ZStack {
-//            Circle()
-//                .frame(width: 200, height: 200)
-//                .foregroundColor(color)
-//            Text("\(number)")
-//                .foregroundColor(.white)
-//                .font(.system(size: 70, weight: .bold))
-//        }
-//    }
+
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         Group {
-            HomeView()
-            CharacterSelectView(userName: "Test")
+//            ResultsView(result: "Draw", score: "23 - 45", username: "Aaron")
+//            HomeView()
+//            CharacterSelectView(userName: "Aaron")
+            MatchView(userName: "Aaron", charNames: ["Joey","Sarah", "Beth"], charID: "2")
         }
     }
 }
